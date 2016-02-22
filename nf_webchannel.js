dc8d9e4e2cea530efe78b04d1483a09bf56ba77a
Yann Flory <yann.flory@xwiki.com>
2016-02-22 11:00:59 +0100

    Fix Netflux API

--- nf_webchannel.js
+++ nf_webchannel.js
@@ -1,34 +1,38 @@
 define(function () {
-    
-    return function WebChannel(id, options) {
-        var module = {exports: {}};
-        var connector = options.connector;
-        var channelId = id;
-        
-        var send = module.exports.send = function(message) {
-            return new Promise(function(resolve, reject) {
-                connector.send(channelId, message).then(function() {
-                    resolve();
-                }, function(error) {
-                    reject(error);
-                });
+
+
+    var module = {exports: {}};
+
+    var connector;
+    var channelId;
+
+    var send = module.exports.send = function(message) {
+        return new Promise(function(resolve, reject) {
+            connector.send(channelId, message).then(function() {
+                resolve();
+            }, function(error) {
+                reject(error);
             });
-            
-        }
+        });
         
-        // Leave the session
-        var leave = module.exports.leave = function() {
-            return new Promise(function(resolve, reject) {
-                try { 
-                    connector.disconnect(); 
-                    resolve();
-                } catch (e) {
-                    reject(e);
-                }
-            });
-        }
-        
-        return module.exports;
-    };
-    
+    }
+
+    // Leave the session
+    var leave = module.exports.leave = function() {
+        return new Promise(function(resolve, reject) {
+            try {
+                connector.disconnect();
+                resolve();
+            } catch (e) {
+                reject(e);
+            }
+        });
+    }
+
+    var create = module.exports.create = function(name, connect) {
+        channelId = name;
+        connector = connect;
+    }
+    return module.exports;
+
 });
